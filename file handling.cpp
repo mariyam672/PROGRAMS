d28379f833ed60e561eb905ec10f24fd4c283e4f
mariyam672 <haroonabid212@gmail.com>
2024-08-23 18:34:31 +0500

    Add files via upload

---
+++ file handling.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+using namespace std;
+int main()
+{// declare output file variable
+ofstream outFile;
+// open an exist file fout.txt
+    outFile.open("fout.txt”);
+
+//behave just like cout, put the word into  the file
+outFile << "Hello World!";
+
+outFile.close();
+
+return 0;
+}
+
+
+
+
